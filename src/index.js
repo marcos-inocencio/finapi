1e88725e56f370898c683076cbcaf8301af9a408
Marcos Inocencio <marcos_inocencio@outlook.com>
2022-10-17 01:21:04 -0300

    Created user registration routes and statement search, created middleware for validation of already registered accounts.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
+const customers = [];
+
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) { 
+    return response.status(400).json({error: 'Customer not found!'}); 
+  }
+
+  request.customer = customer;
+
+  next();
+}
+
+//Routes
 app.post('/account', (request, response) => {
-  const cpf = request.body.cpf;
+  const { cpf, name } = request.body;
+
+  const customerAlredyExists = customers.some(
+    customer => customer.cpf === cpf
+  );
+
+  if(customerAlredyExists) { 
+    return response.status(400).json({error: 'Constumer already exists!'});
+  };
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidV4(),
+    statement: [],
+  });
+
+  response.status(201).send();
 });
 
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.status(200).json(customer.statement);
+});
+
+
+//Server
 app.listen(3333)
